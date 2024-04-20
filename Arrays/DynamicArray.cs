640f146b1e67da2822230201c6d3ca38f0859028
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-04-20 22:35:53 +0300

    Определить класс DynamicArray

---
+++ Arrays/DynamicArray.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Arrays
+{
+    public class DynamicArray : ICloneable
+    {
+        private int[]? _array;
+        public int Size { get; private set; }
+
+        public DynamicArray(object? array)
+        {
+            if (array == null)
+            {
+                _array = null;
+                Size = 0;
+            }
+            if (array is int[] arr)
+            {
+                _array = (int[])arr.Clone();
+                Size = arr.Length;
+            }
+            else
+            {
+                throw new ArgumentException("The argument cannot be cast to a DynamicArray");
+            }
+        }
+        public DynamicArray() : this(null) { }
+        public DynamicArray(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+
+        public object Clone() => this.MemberwiseClone();
+        public IEnumerable GetArray(bool returnReversed = false)
+        {
+            if (_array == null) throw new NullReferenceException("It is not possible to reference a null array");
+            return CurrentImplementation();
+
+            IEnumerable CurrentImplementation()
+            {
+                if (!returnReversed)
+                {
+                    foreach (var item in _array)
+                        yield return item;
+                }
+                else
+                {
+                    for (int i = Size; i != 0; --i)
+                        yield return _array[i - 1];
+                }
+            }
+        }
+        public int GetValue(int index)
+        {
+            if (_array == null) throw new NullReferenceException("It is not possible to reference a null array");
+            if (index < 0 || index >= Size) throw new ArgumentOutOfRangeException("Index invalid");
+            return _array[index];
+        }
+        public void SetValue(int index, int value) { 
+            if (index < 0 || index >= Size) throw new ArgumentOutOfRangeException("index");
+            if (_array == null) throw new NullReferenceException("It is not possible to reference a null array");
+            _array[index] = value;
+        }
+        public void InsertValue(int index, int value)
+        {
+            if (index < 0 || index > Size) throw new ArgumentOutOfRangeException("index");
+            ++Size;
+            if (_array == null)
+            {
+                _array = new int[Size];
+                _array[0] = value;
+            }
+            else
+            {
+                int[] tmpArr = new int[Size];
+                for (int i = 0, j = 0; i < Size; ++i)
+                {
+                    if (i == index)
+                    {
+                        tmpArr[i] = value;
+                        continue;
+                    }
+                    tmpArr[i] = _array[j];
+                    ++j;
+                }
+                
+                _array = tmpArr;
+            }
+        }
+        public void Push(int value)
+        {
+            this.InsertValue(Size - 1, value);
+        }
+        public void Resize(int newSize)
+        {
+            if (newSize < 0) throw new ArgumentException("Size cannot be less than zero");
+            if (_array == null)
+            {
+                _array = new int[newSize];
+            } 
+            if (Size == newSize) return;
+            else if (Size > newSize)
+            {
+                int[] tmpArr = new int[newSize];
+                for (int i = 0; i < newSize; ++i)
+                    tmpArr[i] = _array[i];
+                _array = tmpArr;
+            }
+            else if (Size < newSize)
+            {
+                int[] tmpArr = new int[newSize];
+                for (int i = 0; i < Size; ++i)
+                    tmpArr[i] = _array[i];
+                for (int i = Size; i < newSize; ++i)
+                    tmpArr[i] = default(int);
+                _array = FillDefault(tmpArr, Size, newSize);
+
+            }
+            Size = newSize;
+            static int[] FillDefault(int[] arr, int start, int count)
+            {
+                for (int i = start; i < count; ++i)
+                    arr[i] = default(int);
+                return arr;
+            }
+        } 
+        public void Sort(IComparer comparer)
+        {
+            if (_array == null) throw new NullReferenceException("It is not possible to reference a null array");
+            Array.Sort(_array, comparer);
+        }
+        public override string ToString()
+        {
+            StringBuilder tmp = new StringBuilder();
+            foreach (var item in this.GetArray())
+                tmp.Append("[" + item.ToString() + "] ");
+            return tmp.ToString().TrimEnd();
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) throw new ArgumentNullException($"It is not possible to reference a null {nameof(obj)}");
+            if (obj is DynamicArray arr)
+                return this.ToString() == arr.ToString() ? true : false;
+            throw new ArgumentException($"Unable to convert {nameof(obj)} to DynamicArray");
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        
+    }
+}
